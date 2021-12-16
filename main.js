d2e51c3d279aff3c8cea10c60ddd5c8996cdc5ff
Ahana Chakraborti <82863501+Ahana12345@users.noreply.github.com>
2021-12-16 17:13:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var x= document.getElementById("myAudio");
+function playSound(){
+    x.play();	
+    }
+    
+    var b= document.getElementById("myClip");
+    function playSound1(){
+        b.play();	
+        }
+        var c= document.getElementById("mySong");
+function playSound2(){
+    c.play();	
+    }
+    var a= document.getElementById("myMusic");
+function playSound3(){
+    a.play();	
+    }
+    var v= document.getElementById("myAudio2");
+    function playSound4(){
+        v.play();	
+        }
+function pauseSound(){
+    x.pause();
+    a.pause();
+    c.pause();
+    b.pause();
+    v.pause();
+}
